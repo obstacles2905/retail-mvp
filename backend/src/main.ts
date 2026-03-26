dbbd8f6e107dba429dda58141ed101af41626309
Nikita <pismennikita@gmail.com>
2026-03-26 22:46:26 +0200

    dockerfile fix

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -60,7 +60,7 @@ async function bootstrap(): Promise<void> {
   SwaggerModule.setup('openapi', app, document);
 
   const port = process.env.PORT ?? 3001;
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
 }
 
 void bootstrap();
